f62ed6beaee1bbb8078a9e9ad2f6751ee5367361
George <lunohod0002@gmail.com>
2025-11-03 12:05:43 +0300

    Initial Commit

---
+++ src/main/java/com/example/vkr_api_gateway/RouterConfig.java
@@ -0,0 +1,25 @@
+package com.example.vkr_api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("station-service-id", r -> r
+                        .path("/api/stations/**")
+                        .uri("http://localhost:8081"))
+                .route("media-service-id", r -> r
+                    .path("/api/medias/**")
+                    .uri("http://localhost:8082"))
+                .build();
+    }
+}
+
---
+++ src/main/java/com/example/vkr_api_gateway/VkrApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.vkr_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkrApiGatewayApplication.class, args);
+	}
+
+}

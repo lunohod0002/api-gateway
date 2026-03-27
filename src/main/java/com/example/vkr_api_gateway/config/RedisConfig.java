f236c5e9a68836428ac0e3ca347525353867adc3
George Zhironkin <lunohod0002@gmail.com>
2026-03-28 02:05:53 +0300

    feat: update pom.xml, update RedisConfig

--- src/main/java/com/example/vkr_api_gateway/config/RedisConfig.java
+++ src/main/java/com/example/vkr_api_gateway/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.example.vkr_api_gateway.config;
 
 import com.example.vkr_api_gateway.model.RefreshSession;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
@@ -8,7 +11,6 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-
 @Configuration
 public class RedisConfig {
 
@@ -16,8 +18,14 @@ public class RedisConfig {
     public ReactiveRedisTemplate<String, RefreshSession> refreshSessionRedisTemplate(
             ReactiveRedisConnectionFactory factory
     ) {
-        var keySerializer = new StringRedisSerializer();
-        var valueSerializer = new Jackson2JsonRedisSerializer<>(RefreshSession.class);
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<RefreshSession> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, RefreshSession.class);
 
         RedisSerializationContext<String, RefreshSession> context =
                 RedisSerializationContext.<String, RefreshSession>newSerializationContext(keySerializer)

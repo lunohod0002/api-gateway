2d1456568be8b868c09ab256d35190bd389853df
George Zhironkin <lunohod0002@gmail.com>
2026-04-26 11:12:24 +0300

    update strcture

--- src/main/java/com/example/vkr_api_gateway/security/SecurityConfig.java
+++ src/main/java/com/example/vkr_api_gateway/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.example.vkr_api_gateway.config;
+package com.example.vkr_api_gateway.security;
 
-import com.example.vkr_api_gateway.security.JwtAuthenticationWebFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

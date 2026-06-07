84ebda2e18fe64100dbf7c2cb7f5a12019980777
George Zhironkin <lunohod0002@gmail.com>
2026-06-07 16:08:16 +0300

    feat: add  cors

--- src/main/java/com/example/vkr_api_gateway/security/SecurityConfig.java
+++ src/main/java/com/example/vkr_api_gateway/security/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -25,17 +31,32 @@ public class SecurityConfig {
                 .logout(ServerHttpSecurity.LogoutSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
 
-                        // auth endpoints
                         .pathMatchers(HttpMethod.POST, "/auth/login", "/auth/refresh").permitAll()
                         .pathMatchers(HttpMethod.POST, "/auth/logout").authenticated()
-
-                        // admin only
                         .pathMatchers(HttpMethod.POST, "/api/attractions").hasRole("ADMIN")
                         .pathMatchers(HttpMethod.POST, "/api/medias/upload").hasRole("ADMIN")
-                        // всё остальное публично
                         .anyExchange().permitAll()
                 )
                 .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+         configuration.setAllowedOrigins(List.of( "http://127.0.0.1:5500"));
+
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
     }
 }
